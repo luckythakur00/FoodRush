1473363e375d209a771ca43ad797495b7990ae5c
Lucky <luckythakur13709@gmail.com>
2025-06-17 12:35:29 +0530

    removed loaderio file

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,6 @@ app.use(cors({
 app.use(cookieParser())
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use(express.static('public'));
 
 app.use('/api/user', userRouter)
 app.use('/api/food', foodRoute)
