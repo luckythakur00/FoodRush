df6760d25b8af65795f3ebbbd45f3b0b4d2649aa
Lucky <luckythakur13709@gmail.com>
2025-06-17 11:53:55 +0530

    Add Loader.io verification file

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,6 +21,7 @@ app.use(cors({
 app.use(cookieParser())
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.static('public'));
 
 app.use('/api/user', userRouter)
 app.use('/api/food', foodRoute)
